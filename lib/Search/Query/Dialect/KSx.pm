c0bc9677f0d70ae27bc089079a95c4c8eef644d0
Peter Karman <karpet@peknet.com>
2010-02-23 15:18:35 +0000

    add KSx dialect (subclass of Native for now)

---
+++ lib/Search/Query/Dialect/KSx.pm
@@ -0,0 +1,89 @@
+package Search::Query::Dialect::KSx;
+use strict;
+use warnings;
+use base qw( Search::Query::Dialect::Native );
+use Carp;
+use Data::Dump qw( dump );
+
+our $VERSION = '0.08';
+
+=head1 NAME
+
+Search::Query::Dialect::KSx - KinoSearch query dialect
+
+=head1 SYNOPSIS
+
+ my $query = Search::Query->parser( dialect => 'KSx' )->parse('foo');
+ print $query;
+
+=head1 DESCRIPTION
+
+Search::Query::Dialect::KSx supports the KinoSearch::QueryParser syntax.
+
+=head1 METHODS
+
+This class is a subclass of Search::Query::Dialect::Native. 
+Only new or overridden methods are documented here.
+
+=cut
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Peter Karman, C<< <karman at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-search-query at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Search-Query>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Search::Query
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Search-Query>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Search-Query>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Search-Query>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Search-Query/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+This module started as a fork of Search::QueryParser by
+Laurent Dami.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Peter Karman.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
